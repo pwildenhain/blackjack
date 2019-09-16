668a75cf30155faba2be73b414be4ca491eda1a5
pwildenhain <pwildenhain@gmail.com>
2019-09-15 21:42:29 -0400

    :speak_no_evil: Silence pylint

--- blackjack/roles.py
+++ blackjack/roles.py
@@ -1,7 +1,7 @@
 """Roles for playing blackjack.
 Includes base class, with player and dealer implementation.
 """
-from typing import Union
+
 from terminal_playing_cards import View, Card
 
 
@@ -18,21 +18,22 @@ class Role:
         """Set up the role, and calculate the blackjack hand starting total."""
         self._hand = None
         self.hand = hand
+        self.total = None
 
     @property
+    # pylint: disable=missing-docstring
     def hand(self):
         return self._hand
+    # pylint: enable=missing-docstring
 
     @hand.setter
     def hand(self, value: View):
         """Calculate the total when a View is added to the hand"""
-        # Skip setting if the hand is empty
-        if not value:
-            return None
-
         self._hand = value
-        self.set_ace_value()
-        self.total = sum(self._hand)
+        # Set other attributes if hand isn't None
+        if value:
+            self.set_ace_value()
+            self.total = sum(self._hand)
 
     def set_ace_value(self) -> None:
         """Re-evaluate the current value for an ace"""
