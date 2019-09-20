c7dd7d28a5d125ca540385d04e7233d21283e258
pwildenhain <pwildenhain@gmail.com>
2019-09-19 22:17:04 -0400

    :recycle: Modularize pytest fixtures

--- tests/test_roles.py
+++ tests/test_roles.py
@@ -12,16 +12,24 @@ from blackjack.roles import Role
 def non_ace_hand():
     from terminal_playing_cards import View, Card
 
-    cards = View([Card("5", "hearts", value=5), Card("K", "hearts", value=10)])
-    return Role(hand=cards)
+    return View([Card("5", "hearts", value=5), Card("K", "hearts", value=10)])
 
 
 @pytest.fixture
 def ace_hand():
     from terminal_playing_cards import View, Card
 
-    cards = View([Card("A", "hearts", value=1), Card("K", "hearts", value=10)])
-    return Role(hand=cards)
+    return View([Card("A", "hearts", value=1), Card("K", "hearts", value=10)])
+
+
+@pytest.fixture
+def role_without_ace(non_ace_hand):
+    return Role(hand=non_ace_hand)
+
+
+@pytest.fixture
+def role_with_ace(ace_hand):
+    return Role(hand=ace_hand)
 
 
 @pytest.fixture
@@ -31,19 +39,19 @@ def queen_spades():
     return Card("Q", "spades", value=10)
 
 
-def test_hand_total_value(non_ace_hand, ace_hand):
-    assert non_ace_hand.total == 15
-    assert ace_hand.total == 21
+def test_hand_total_value(role_without_ace, role_with_ace):
+    assert role_without_ace.total == 15
+    assert role_with_ace.total == 21
 
 
-def test_hit_method_adds_to_total(non_ace_hand, queen_spades):
-    non_ace_hand.hit(card=queen_spades)
-    assert non_ace_hand.total == 25
+def test_hit_method_adds_to_total(role_without_ace, queen_spades):
+    role_without_ace.hit(card=queen_spades)
+    assert role_without_ace.total == 25
 
 
-def test_hit_method_adjusts_ace_value(ace_hand, queen_spades):
-    ace_hand.hit(card=queen_spades)
-    assert ace_hand.total == 21
+def test_hit_method_adjusts_ace_value(role_with_ace, queen_spades):
+    role_with_ace.hit(card=queen_spades)
+    assert role_with_ace.total == 21
 
 
 def test_role_can_be_created_without_cards():
