55f48c01b33f582e3e94924e6315936dae0eb381
pwildenhain <pwildenhain@gmail.com>
2019-10-26 14:25:08 -0400

    :white_check_mark: Add tests for Blackjack.deal() method

--- tests/test_blackjack.py
+++ tests/test_blackjack.py
@@ -9,14 +9,12 @@ from blackjack.game import Blackjack
 
 
 @pytest.fixture
-def new_blackjack_game(): 
+def new_blackjack_game():
     from terminal_playing_cards import Deck
     from blackjack.roles import Player, Dealer
 
     return Blackjack(
-        deck=Deck(),
-        dealer=Dealer(),
-        players=[Player(bank=500), Player(bank=500)],
+        deck=Deck(), dealer=Dealer(), players=[Player(bank=500), Player(bank=500)]
     )
 
 
@@ -56,3 +54,24 @@ def test_rejecting_bets_that_are_too_big(new_blackjack_game, mocker):
     assert bet_mock.call_count == 3
     assert new_blackjack_game.players[0].bet == 500
     assert new_blackjack_game.players[1].bet == 500
+
+
+def test_cards_are_dealt_to_players(new_blackjack_game):
+    new_blackjack_game.deal()
+    players_have_two_cards = [
+        len(player.hand) == 2 for player in new_blackjack_game.players
+    ]
+    assert all(players_have_two_cards)
+
+
+def test_cards_are_dealt_to_dealer(new_blackjack_game):
+    new_blackjack_game.deal()
+    assert len(new_blackjack_game.dealer.hand) == 2
+
+
+def test_dealer_second_card_is_hidden(new_blackjack_game):
+    new_blackjack_game.deal()
+    dealer_hand = new_blackjack_game.dealer.hand
+
+    assert not dealer_hand[0].hidden
+    assert dealer_hand[1].hidden
